fb11ab9d41f06c18ad2a203e34dea6408da7f588
Hang <hang.chsa@gmail.com>
2025-01-01 14:24:46 +0800

    Upgrade react, nextjs version. Fix safari cannot set cookies in development mode

--- src/utils/sesstion.ts
+++ src/utils/sesstion.ts
@@ -6,7 +6,6 @@ import { API_RefreshTokens } from "@/types/api/refreshTokens";
 
 export async function sessionCookies(){
   const cookiesFn = await cookies();
-
   const get = () => {
     const accessToken = cookiesFn.get('sid')?.value;
     const refreshToken = cookiesFn.get('rsid')?.value;
@@ -15,7 +14,7 @@ export async function sessionCookies(){
   const set = (accessToken:string,refreshToken:string) => {
     const secureOption = {
       httpOnly:true,
-      secure:true,
+      secure: process.env.NODE_ENV == 'development' ? false : true,
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
     }
     cookiesFn.set('sid',accessToken, secureOption);
